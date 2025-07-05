eac0509d4a8e0c4abab2e8bc4b066bb47ea86126
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2025-07-05 15:14:44 +0600

    php initiate

---
+++ admin/manage-users.php
@@ -0,0 +1,93 @@
+<?php
+include 'partials/header.php';
+?>
+<section class="dashboard">
+  <div class="container dashboard_container">
+    <button id="show_sidebar-btn" class="sidebar_toggle">
+      <i class="uil uil-angle-right-b"></i>
+    </button>
+    <button id="hide_sidebar-btn" class="sidebar_toggle">
+      <i class="uil uil-angle-left-b"></i>
+    </button>
+    <aside>
+      <ul>
+        <li>
+          <a href="add-post.html"><i class="uil uil-pen"></i>
+            <h5>Add Post</h5>
+          </a>
+        </li>
+        <li>
+          <a href="dashboard.html"><i class="uil uil-postcard"></i>
+            <h5>Manage Posts</h5>
+          </a>
+        </li>
+        <li>
+          <a href="add-user.html"><i class="uil uil-user-plus"></i>
+            <h5>Add User</h5>
+          </a>
+        </li>
+        <li>
+          <a href="manage-users.html" class="active"><i class="uil uil-users-alt"></i>
+            <h5>Manage Users</h5>
+          </a>
+        </li>
+        <li>
+          <a href="add-category.html"><i class="uil uil-edit"></i>
+            <h5>Add Category</h5>
+          </a>
+        </li>
+        <li>
+          <a href="manage-categories.html"><i class="uil uil-list-ul"></i>
+            <h5>Manage Categories</h5>
+          </a>
+        </li>
+      </ul>
+    </aside>
+    <main>
+      <h2>Manage Users</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Edit</th>
+            <th>Delete</th>
+            <th>Admin</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Kabir</td>
+            <td>kabir4874</td>
+            <td><a href="edit-user.html" class="btn sm">Edit</a></td>
+            <td>
+              <a href="delete-user.html" class="btn sm danger">Delete</a>
+            </td>
+            <td>Yes</td>
+          </tr>
+          <tr>
+            <td>Kabir</td>
+            <td>kabir4874</td>
+            <td><a href="edit-user.html" class="btn sm">Edit</a></td>
+            <td>
+              <a href="delete-user.html" class="btn sm danger">Delete</a>
+            </td>
+            <td>Yes</td>
+          </tr>
+          <tr>
+            <td>Kabir</td>
+            <td>kabir4874</td>
+            <td><a href="edit-category.html" class="btn sm">Edit</a></td>
+            <td>
+              <a href="delete-category.html" class="btn sm danger">Delete</a>
+            </td>
+            <td>Yes</td>
+          </tr>
+        </tbody>
+      </table>
+    </main>
+  </div>
+</section>
+<?php
+include '../partials/footer.php'
+?>
---
+++ partials/header.php
@@ -0,0 +1,44 @@
+<?php
+require 'config/database.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Responsive Multi Page Website</title>
+    <!-- ?custom css  -->
+    <link rel="stylesheet" href="styles/style.css" />
+    <!-- ?unicons cdn -->
+    <link
+        rel="stylesheet"
+        href="https://unicons.iconscout.com/release/v4.0.8/css/line.css" />
+</head>
+
+<body>
+    <!-- !navbar -->
+    <nav>
+        <div class="container nav_container">
+            <a href="<?= ROOT_URL ?>index.php" class="nav_logo">KABIR</a>
+            <ul class="nav_items">
+                <li><a href="<?= ROOT_URL ?>blog.php">Blog</a></li>
+                <li><a href="<?= ROOT_URL ?>about.php">About</a></li>
+                <li><a href="<?= ROOT_URL ?>services.php">Services</a></li>
+                <li><a href="<?= ROOT_URL ?>contact.php">Contact</a></li>
+                <li><a href="<?= ROOT_URL ?>signin.php">Signin</a></li>
+                <li class="nav_profile">
+                    <div class="avatar">
+                        <img src="./assets/avatar1.jpg" alt="avatar" />
+                    </div>
+                    <ul>
+                        <li><a href="<?= ROOT_URL ?>admin/index.php">Dashboard</a></li>
+                        <li><a href="<?= ROOT_URL ?>admin/logout.php">Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <button id="open_nav-btn"><i class="uil uil-bars"></i></button>
+            <button id="close_nav-btn"><i class="uil uil-multiply"></i></button>
+        </div>
+    </nav>
