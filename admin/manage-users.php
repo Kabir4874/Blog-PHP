0dff446c2fbb8712e7b75fd782f504282ed28f13
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2025-07-10 20:05:47 +0600

    manage user done

--- admin/manage-users.php
+++ admin/manage-users.php
@@ -1,5 +1,11 @@
 <?php
 include 'partials/header.php';
+
+// fetch users from database but not current user 
+$current_admin_id = $_SESSION['user-id'];
+
+$query = "SELECT * FROM users WHERE NOT id=$current_admin_id";
+$result = mysqli_query($connection, $query);
 ?>
 <section class="dashboard">
   <div class="container dashboard_container">
@@ -58,33 +64,17 @@ include 'partials/header.php';
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Kabir</td>
-            <td>kabir4874</td>
-            <td><a href="edit-user.php" class="btn sm">Edit</a></td>
-            <td>
-              <a href="delete-user.php" class="btn sm danger">Delete</a>
-            </td>
-            <td>Yes</td>
-          </tr>
-          <tr>
-            <td>Kabir</td>
-            <td>kabir4874</td>
-            <td><a href="edit-user.php" class="btn sm">Edit</a></td>
-            <td>
-              <a href="delete-user.php" class="btn sm danger">Delete</a>
-            </td>
-            <td>Yes</td>
-          </tr>
-          <tr>
-            <td>Kabir</td>
-            <td>kabir4874</td>
-            <td><a href="edit-category.php" class="btn sm">Edit</a></td>
-            <td>
-              <a href="delete-category.php" class="btn sm danger">Delete</a>
-            </td>
-            <td>Yes</td>
-          </tr>
+          <?php while ($user = mysqli_fetch_assoc($result)): ?>
+            <tr>
+              <td><?= $user['firstname'] . " " . $user['lastname'] ?></td>
+              <td><?= $user['username'] ?></td>
+              <td><a href="<?= ROOT_URL ?>admin/edit-user.php?id=<?= $user['id'] ?>" class="btn sm">Edit</a></td>
+              <td>
+                <a href="<?= ROOT_URL ?>admin/delete-user.php" class="btn sm danger">Delete</a>
+              </td>
+              <td><?= $user['is_admin'] == 0 ? "No" : "Yes" ?></td>
+            </tr>
+          <?php endwhile ?>
         </tbody>
       </table>
     </main>
