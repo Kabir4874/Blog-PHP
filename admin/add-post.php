7b31d54e2f0658e9d61b20451e6f4ab7f36c882d
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2025-07-19 22:07:23 +0600

    edit & delete post done

--- admin/add-post.php
+++ admin/add-post.php
@@ -4,23 +4,35 @@ include 'partials/header.php';
 // fetch categories 
 $query = "SELECT * FROM categories";
 $categories = mysqli_query($connection, $query);
+
+// get back form data if form was invalid 
+$title = $_SESSION['add-post-data']['title'] ?? null;
+$body = $_SESSION['add-post-data']['body'] ?? null;
+
+unset($_SESSION['add-post-data']);
 ?>
 
 <!-- ! add post   -->
 <section class="form_section">
   <div class="container form_section-container">
     <h2>Add Post</h2>
-    <div class="alert_message error">
-      <p>This is an error message</p>
-    </div>
+    <?php
+    if (isset($_SESSION['add-post'])): ?>
+      <div class="alert_message error">
+        <p>
+          <?= $_SESSION['add-post'];
+          unset($_SESSION['add-post']) ?>
+        </p>
+      </div>
+    <?php endif ?>
     <form action="<?= ROOT_URL ?>admin/logic/add-post-logic.php" enctype="multipart/form-data" method="POST">
-      <input type="text" name="title" placeholder="Title" />
+      <input type="text" name="title" placeholder="Title" value="<?= $title ?>" />
       <select name="category">
         <?php while ($category = mysqli_fetch_assoc($categories)): ?>
           <option value="<?= $category['id'] ?>"><?= $category['title'] ?></option>
         <?php endwhile ?>
       </select>
-      <textarea rows="10" name="body" placeholder="Body"></textarea>
+      <textarea rows="10" name="body" placeholder="Body"><?= htmlspecialchars($body) ?></textarea>
       <?php if (isset($_SESSION['user_is_admin'])): ?>
         <div class="form_control inline">
           <input type="checkbox" name="is_featured" value="1" id="is_featured" checked />
--- admin/edit-post.php
+++ admin/edit-post.php
@@ -1,36 +1,84 @@
 <?php
 include 'partials/header.php';
+
+// fetch categories 
+$query = "SELECT * FROM categories";
+$categories = mysqli_query($connection, $query);
+
+// fetch post data from database if id is set 
+if (isset($_GET['id'])) {
+  $id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
+  $query = "SELECT * FROM posts WHERE id=$id";
+  $result = mysqli_query($connection, $query);
+
+  if (mysqli_num_rows($result) == 1) {
+    $post = mysqli_fetch_assoc($result);
+  } else {
+    $_SESSION['edit-post'] = "Post not found";
+    header('location: ' . ROOT_URL . 'admin/index.php');
+    die();
+  }
+} else {
+  header('location: ' . ROOT_URL . 'admin/index.php');
+  die();
+}
+
+// get back form data if form was invalid
+$title = $_SESSION['edit-post-data']['title'] ?? $post['title'];
+$body = $_SESSION['edit-post-data']['body'] ?? $post['body'];
+$category_id = $_SESSION['edit-post-data']['category'] ?? $post['category_id'];
+$is_featured = $_SESSION['edit-post-data']['is_featured'] ?? $post['is_featured'];
+
+// delete form data session
+unset($_SESSION['edit-post-data']);
 ?>
 
 <!-- ! edit post   -->
 <section class="form_section">
   <div class="container form_section-container">
     <h2>Edit Post</h2>
-    <div class="alert_message error">
-      <p>This is an error message</p>
-    </div>
-    <form action="" enctype="multipart/form-data">
-      <input type="text" placeholder="Title" />
-      <select>
-        <option value="1">Travel</option>
-        <option value="1">Art</option>
-        <option value="1">Science & Technology</option>
-        <option value="1">Travel</option>
-        <option value="1">Travel</option>
-      </select>
-      <textarea rows="10" placeholder="Body"></textarea>
-      <div class="form_control inline">
-        <input type="checkbox" id="is_featured" checked />
-        <label for="is_featured">Featured</label>
+
+    <?php if (isset($_SESSION['edit-post'])): ?>
+      <div class="alert_message error">
+        <p>
+          <?= $_SESSION['edit-post'];
+          unset($_SESSION['edit-post']) ?>
+        </p>
       </div>
+    <?php endif ?>
+
+    <form action="<?= ROOT_URL ?>admin/logic/edit-post-logic.php" enctype="multipart/form-data" method="POST">
+      <input type="hidden" name="id" value="<?= $post['id'] ?>">
+      <input type="text" name="title" value="<?= $title ?>" placeholder="Title">
+
+      <select name="category">
+        <?php while ($category = mysqli_fetch_assoc($categories)): ?>
+          <option value="<?= $category['id'] ?>" <?= $category['id'] == $category_id ? 'selected' : '' ?>>
+            <?= $category['title'] ?>
+          </option>
+        <?php endwhile ?>
+      </select>
+
+      <textarea rows="10" name="body" placeholder="Body"><?= htmlspecialchars($body) ?></textarea>
+
+      <?php if (isset($_SESSION['user_is_admin'])): ?>
+        <div class="form_control inline">
+          <input type="checkbox" name="is_featured" id="is_featured" value="1" <?= $is_featured ? 'checked' : '' ?>>
+          <label for="is_featured">Featured</label>
+        </div>
+      <?php endif ?>
+
       <div class="form_control">
         <label for="thumbnail">Change Thumbnail</label>
-        <input type="file" id="thumbnail" />
+        <input type="file" name="thumbnail" id="thumbnail">
       </div>
-      <button type="submit" class="btn">Update Post</button>
+
+      <input type="hidden" name="old_thumbnail" value="<?= $post['thumbnail'] ?>">
+      <button type="submit" name="submit" class="btn">Update Post</button>
     </form>
   </div>
 </section>
+
 <?php
-include '../partials/footer.php'
+include '../partials/footer.php';
 ?>
